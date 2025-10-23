32573a7a26c48710eaf1a775c6000bce0d6bb93e
loedetyler <loedetyler@gmail.com>
2025-10-23 08:25:03 -0500

    Configured dockerfile, docker-compose, requirements and settings to all support the microservice architecture with web and db

--- dogapiexercise/settings.py
+++ dogapiexercise/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dogapiexercise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogdb',
+        'USER': 'doguser',
+        'PASSWORD': 'dogpass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
